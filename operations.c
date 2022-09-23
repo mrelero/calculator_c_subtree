30f17b05e37f0a85d4dc30b91b24292b72d85d1b
Matheus Raphael Elero <matheus.elero@setis>
2022-09-23 15:57:33 -0300

    Squashed 'math_operations_h/' content from commit fd3f7b8
    git-subtree-dir: math_operations_h
    git-subtree-split: fd3f7b883d04387a6c3c79e027e33f7c4120915c

---
+++ operations.c
@@ -0,0 +1,66 @@
+#include "operations.h"
+
+
+OPR_iSoma(int iNum1, int iNum2){
+   return iNum1 + iNum2;
+}
+
+
+
+int OPR_iProduto(int iNum1, int iNum2){
+   return iNum1 * iNum2;
+}
+
+int OPR_iPotencia(int iNum1, int iExp){
+   int iAux;
+   int iMult;
+   iAux = 1;
+   for(iMult = 1; iMult <= iExp; iMult++){
+      iAux = iNum1 * iAux;
+   }
+   return iAux;
+}
+
+extern long OPR_lFatorial(long lNumber){
+   long lAux;
+   long lIndex;
+   lAux = 1;
+   for(lIndex = 1; lIndex <= lNumber; lIndex++){
+      lAux = lIndex * lAux;
+   }
+
+   return lAux;
+}
+
+extern int OPR_iModulo(int iNumber){
+
+   if(iNumber < 0){
+      return (iNumber * -1);
+   }
+
+   return iNumber;
+}
+
+extern int OPR_iIsOdd(int iNumber){
+
+   if(iNumber%2 == 0){
+      return 0;
+   }
+
+   return 1;
+}
+
+extern float OPR_fDiv(float fDenominador, float fDividendo){
+   return fDenominador / fDividendo;
+}
+
+
+extern long OPR_l10Exp(int iExp){
+   long lAux;
+   int iMult;
+   lAux = 1;
+   for (iMult = 1; iMult <= iExp; iMult++) {
+      lAux = 10 * lAux;
+   }
+   return lAux;
+}
---
+++ operations.h
@@ -0,0 +1,29 @@
+#ifndef _OPERATIONS_INCLUDED_
+#define _OPERATIONS_INCLUDED_
+
+
+#include <stdio.h>
+
+extern int OPR_iSoma(int iNum1, int iNum2);
+
+extern int OPR_iProduto(int iNum1, int iNum2);
+
+extern int OPR_iPotencia(int iNum1, int iExp);
+
+extern long OPR_lFatorial(long lNumber);
+
+extern int OPR_iModulo(int iNumber);
+
+extern int OPR_iIsOdd(int iNumber);
+
+extern float OPR_fDiv(float fDenominador, float fDividendo);
+
+extern float OPR_fDiv(float fDenominador, float fDividendo);
+
+extern long OPR_l10Exp(int iExp);
+
+#endif /* _CRT_SEC_INCLUDED_ */
+
+
+
+
